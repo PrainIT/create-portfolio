cacba8c0d5c25c09e6c5a2c48dba237a6855c696
zino.kim <yasisicoco@gmail.com>
2025-12-11 17:16:52 +0900

    Remove duplicate public and src folders from root directory

--- src/app/contact/page.tsx
+++
@@ -1,8 +0,0 @@
-export default async function ContactPage() {
-  return (
-    <main className="w-full">
-      <div className="w-full h-px bg-grey-700 mt-12" />        
-      <div>ㅎㅇ</div>
-    </main>
-  );
-}
